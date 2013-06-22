abcdaac30807c4df123989050a4b3b57af73652e
Julio Protzek <julioprotzek@gmail.com>
2013-06-22 07:25:51 -0300

    fist commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gotadagua::Application.config.secret_key_base = '816a0da8c4cf8a4e6b4319cbd530ea3b98e38d32078eb816eed442fe6d86c25b455990cc173f7945e3a3df7d65b713d671187cc1ac415d3e1cfc8322af05349f'

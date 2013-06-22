cc012bf415cdce9bebf81b085a4b2dc8de098fa1
Julio Protzek <julioprotzek@gmail.com>
2013-06-22 09:42:26 -0300

    add new motives

--- app/controllers/motives_controller.rb
+++ app/controllers/motives_controller.rb
@@ -2,4 +2,26 @@ class MotivesController < ApplicationController
   def index
     @motives = Motive.all
   end
+
+  def new
+    @motive = Motive.new
+  end
+
+  def create
+    @motive = Motive.new(motive_params)
+    if @motive.save
+      redirect_to @motive
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @motive = Motive.find(params[:id])
+  end
+
+  private
+    def motive_params
+      params.require(:motive).permit(:description, :tag_id)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Gotadagua::Application.routes.draw do
   root 'motives#index'
+  resources :motives, only: [:new, :create, :show]
 end
